e03f34ea1a8c3430526a096cb90f3301805ce834
Luis <lruizarc@msudenver.edu>
2020-03-25 19:43:52 -0600

    merged all three pages

---
+++ app/src/main/java/com/example/last_seen_rescuer/BluetoothPage.kt
@@ -0,0 +1,51 @@
+package com.example.last_seen_rescuer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+
+class BluetoothPage : AppCompatActivity() {
+
+    private lateinit var profileInformation : TextView
+
+    private lateinit var bluetoothLogListView : ListView
+    private var temporary_dummy_data = 0
+
+    private lateinit var bluetoothLogAdapter : ArrayAdapter<String>
+    private lateinit var bluetoothLogArrayList : ArrayList<String>
+
+    private lateinit var getDataButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth_page)
+
+        initializeViewsAndAdapters()
+
+        // TODO : PULL EXTRAS FROM INTENT AND POPULATE PROFILE INFORMATION
+
+        getDataButton.setOnClickListener {
+            bluetoothLogArrayList.add("TEMPORARY LOG DATA " + temporary_dummy_data)
+            bluetoothLogAdapter.notifyDataSetChanged()
+
+            temporary_dummy_data++
+
+            // TODO : GET DATA FROM BLUETOOTH CHECKPOINTS FOR THE GIVEN MAC ADDRESS
+        }
+    }
+
+    private fun initializeViewsAndAdapters() {
+        bluetoothLogListView = findViewById(R.id.bluetooth_checkpoint_data_list_view)
+        bluetoothLogArrayList = ArrayList()
+        bluetoothLogAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, bluetoothLogArrayList)
+
+        bluetoothLogListView.adapter = bluetoothLogAdapter
+
+        getDataButton = findViewById(R.id.get_data_button)
+
+        profileInformation = findViewById(R.id.profile_information_text_view)
+    }
+}
---
+++ app/src/main/java/com/example/last_seen_rescuer/SelectionPage.kt
@@ -0,0 +1,54 @@
+package com.example.last_seen_rescuer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+/*
+ * ALL VARIABLES AND FUNCTIONS WITH THE WORD TEMPORARY ONLY HERE FOR DEMO PURPOSES
+ * UNTIL WE CONNECT THE MOBILE APP TO THE BACKEND
+ */
+
+class SelectionPage : AppCompatActivity() {
+
+    private lateinit var profileListView : ListView
+    private lateinit var temporary_generateProfileButton : Button
+    private var temporary_id = 0
+
+    private lateinit var profileAdapter: ArrayAdapter<String>
+    private lateinit var profileArrayList: ArrayList<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection_page)
+
+        initializeViewsAndAdapters()
+
+        // TODO: PULL EXTRAS FROM THE INTENT AND POPULATE THE SCROLLVIEW
+
+        temporary_generateProfileButton.setOnClickListener {
+            profileArrayList.add("PROFILE NUMBER " + temporary_id)
+            profileAdapter.notifyDataSetChanged()
+
+            temporary_id++;
+        }
+
+        profileListView.setOnItemClickListener {parent, view, position, id ->
+            val profile = profileAdapter.getItem(position)
+
+            Toast.makeText(applicationContext, profile, Toast.LENGTH_SHORT).show()
+
+            // TODO: START CHECKPOINT PAGE WITH INTENT CONTAINING PROFILE INFORMATION
+        }
+    }
+
+    private fun initializeViewsAndAdapters() {
+        profileListView = findViewById(R.id.profile_list_view)
+        profileArrayList = ArrayList()
+        profileAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, profileArrayList)
+
+        profileListView.adapter = profileAdapter
+
+        temporary_generateProfileButton = findViewById(R.id.temporary_add_profile_button)
+    }
+}
